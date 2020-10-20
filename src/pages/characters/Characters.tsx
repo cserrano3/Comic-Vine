929b840f65c763717af30bc03a899e7c54fae333
carolina <carolina.f.serrano.l@gmail.com>
2020-10-20 07:05:31 -0300

    test

---
+++ src/pages/characters/Characters.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import CharacterList from '../../components/organisms/CharacterList/CharacterList';
+
+export default function Characters() {
+    return (
+        <CharacterList />
+    )
+}
